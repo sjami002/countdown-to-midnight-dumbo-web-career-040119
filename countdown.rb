ff292277412845b3da33e900fd8410180ff24fc8
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-13 17:56:18 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  counter = int
+  while counter > 0
+    puts "#{int} SECOND(S)!"
+    counter -=
+  end
+  puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  counter = int
+  while counter > 0
+    puts "#{int}sleep 5 SECOND(S)!"
+    counter -=
+  end
+  puts "HAPPY NEW YEAR!"
 end
