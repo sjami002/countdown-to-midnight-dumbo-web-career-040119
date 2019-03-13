67976147e06c6a054111e9178d9a4162dfa29d5e
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-13 17:55:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(int)
   end
   puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+  counter = int
+  while counter > 0
+    puts "#{int}sleep 5 SECOND(S)!"
+    counter -=
+  end
+  puts "HAPPY NEW YEAR!"
+end
